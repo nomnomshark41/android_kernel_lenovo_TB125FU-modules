72a38eee6bd0c1b30239bae7446989d81f150858
Neco Arc <necoarc007@proton.me>
2024-12-28 22:12:55 +0100

    trying something

--- connectivity/wlan/core/gen4m/Android.mk
+++ connectivity/wlan/core/gen4m/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(MTK_WLAN_SUPPORT), yes)
 
-ifeq ($(WLAN_BUILD_COMMON), true)
+ifeq ($(WLAN_BUILD_COMMON),)
 	WIFI_NAME := wlan_drv_gen4m
 	WIFI_HIF := axi
 	WIFI_WMT := y
