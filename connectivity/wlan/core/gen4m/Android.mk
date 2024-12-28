d70b63230797286cde62ce1459a9b3d731e80f5a
Neco Arc <necoarc007@proton.me>
2024-12-28 22:00:44 +0100

    refactor gen4m build system: enhance Android.mk for multiple WLAN configurations and introduce build_wlan_drv.mk

--- connectivity/wlan/core/gen4m/Android.mk
+++ connectivity/wlan/core/gen4m/Android.mk
@@ -2,18 +2,80 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(MTK_WLAN_SUPPORT), yes)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := wlan_drv_gen4m.ko
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_OWNER := mtk
-LOCAL_REQUIRED_MODULES := wmt_chrdev_wifi.ko
+ifeq ($(WLAN_BUILD_COMMON), true)
+	# for layer decoupling 2.0, we have to build all configurations
+	WLAN_CHIP_ID := 6893
+	WIFI_CHIP := CONNAC2X2_SOC3_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6893
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
 
-include $(MTK_KERNEL_MODULE)
+	WLAN_CHIP_ID := 6983
+	WIFI_CHIP := CONNAC2X2_SOC7_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6983
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
 
-WIFI_NAME := wlan_drv_gen4m
-WIFI_OPTS := CONFIG_MTK_COMBO_WIFI_HIF=$(WIFI_HIF) MODULE_NAME=$(WIFI_NAME) MTK_COMBO_CHIP=$(WIFI_CHIP) WLAN_CHIP_ID=$(WLAN_CHIP_ID) MTK_ANDROID_WMT=$(WIFI_WMT) WIFI_ENABLE_GCOV=$(WIFI_ENABLE_GCOV) WIFI_IP_SET=$(WIFI_IP_SET) MTK_ANDROID_EMI=$(WIFI_EMI)
-WIFI_OPTS += MTK_WLAN_SERVICE=yes
+	WLAN_CHIP_ID := 6879
+	WIFI_CHIP := CONNAC2X2_SOC7_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6879
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
 
-$(linked_module): OPTS += $(WIFI_OPTS)
+	WLAN_CHIP_ID := 6895
+	WIFI_CHIP := CONNAC2X2_SOC7_0
+	WIFI_IP_SET := 1
+	CONNAC_VER := 2_0
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6895
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi_connac2.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
+
+	WLAN_CHIP_ID := 6855
+	WIFI_CHIP := SOC2_1X1
+	WIFI_IP_SET := 1
+	CONNAC_VER :=
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6855
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
+
+	WLAN_CHIP_ID := 6789
+	WIFI_CHIP := SOC2_1X1
+	WIFI_IP_SET := 1
+	CONNAC_VER :=
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_NAME := wlan_drv_gen4m_6789
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
+else
+	WIFI_NAME := wlan_drv_gen4m
+	WIFI_HIF := axi
+	WIFI_WMT := y
+	WIFI_EMI := y
+	WIFI_CHRDEV_MODULE := wmt_chrdev_wifi.ko
+	include $(LOCAL_PATH)/build_wlan_drv.mk
+endif
 
 endif
